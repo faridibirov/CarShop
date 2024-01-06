3326d77ba8fca61c92baf2b8059f77b412673c00
Farid Dibirov <farid962@gmail.com>
2024-01-06 04:08:29 +0400

    added home controller & categories

--- Shop/Controllers/CarsController.cs
+++ Shop/Controllers/CarsController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Shop.Data.Interfaces;
+using Shop.Data.Models;
 using Shop.ViewModels;
 
 namespace Shop.Controllers;
@@ -15,14 +16,42 @@ public class CarsController : Controller
         _allCategories = allCategories;
     }
 
-
-    public ViewResult List ()
+    [Route("Cars/List")]
+    [Route("Cars/List/{category}")]
+    public ViewResult List (string category)
     {
-        ViewBag.Title = "Страница с Автомобилями";
-        CarListViewModel obj = new CarListViewModel ();
-        obj.AllCars = _allCars.AllCars;
-        obj.currCategory = "Автомобили";
+        string _category = category;
+        IEnumerable<Car> cars = null ;
+        string currCategory = "";
+        if(string.IsNullOrEmpty(category))
+        {
+            cars = _allCars.AllCars.OrderBy(i => i.Id);
+        }
+        else
+        {
+            if (string.Equals("electro", category, StringComparison.OrdinalIgnoreCase))
+            {
+                cars = _allCars.AllCars.Where(i => i.Category.categoryName.Equals("Электромобили")).OrderBy(i => i.Id);
+				currCategory = "Электромобили";
+			}
 
-        return View(obj);
+            else if (string.Equals("fuel", category, StringComparison.OrdinalIgnoreCase))
+			{
+				cars = _allCars.AllCars.Where(i => i.Category.categoryName.Equals("Классические автомобили")).OrderBy(i => i.Id);
+				currCategory = "Классические автомобили";
+			}
+
+          
+		}
+
+        var carObj = new CarListViewModel
+        {
+            AllCars = cars,
+            currCategory = currCategory,
+        };
+
+        ViewBag.Title = "Страница с Автомобилями";
+
+        return View(carObj);
     }
 }
---
+++ Shop/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Shop.Data.Interfaces;
+using Shop.ViewModels;
+
+namespace Shop.Controllers;
+
+public class HomeController : Controller
+{
+	private readonly IAllCars _carRep;
+
+	public HomeController(IAllCars carRep)
+	{
+		_carRep = carRep;
+
+	}
+
+	public ViewResult Index ()
+	{
+		var homeCars = new HomeViewModel
+		{
+			favCars = _carRep.getFavCars
+		};
+
+		return View(homeCars);
+	}
+}
